70f3d8b6b0e00cd11c9e882bbe9b799fcb791aaf
Sayed Malawi <sayed@dhcp-130-58-165-124.swarthmore.edu>
2017-02-06 12:08:16 -0500

    adds album to playlist, descending energy

--- playgen-1.py
+++ playgen-1.py
@@ -5,56 +5,37 @@ import spotipy.util as util
 ''' shows the albums and tracks for a given artist.
 '''
 
-def get_artist(name):
-    results = sp.search(q='artist:' + name, type='artist')
-    items = results['artists']['items']
-    if len(items) > 0:
-        return items[0]
-    else:
-        return None
+#if __name__ == '__main__':
+scope = 'playlist-modify-public'
 
-def show_artist_albums(artist):
-    albums = []
-    results = sp.artist_albums(artist['id'], album_type='album')
-    albums.extend(results['items'])
-    while results['next']:
-        results = sp.next(results)
-        albums.extend(results['items'])
-    seen = set() # to avoid dups
-    albums.sort(key=lambda album:album['name'].lower())
-    for album in albums:
-        name = album['name']
-        if name not in seen:
-            print((' ' + name))
-            seen.add(name)
+if len(sys.argv) == 4:
+    username = sys.argv[1]
+    print username
+    playlist_id = sys.argv[2]
+    album_id = sys.argv[3]
+else:
+    print "Usage: %s username playlist_id album_id" % (sys.argv[0],)
+    sys.exit()
 
-if __name__ == '__main__':
-    scope = 'user-library-read'
+token = util.prompt_for_user_token(username, scope)
 
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        print "Usage: %s username" % (sys.argv[0],)
-        sys.exit()
+if token:
+    sp = spotipy.Spotify(auth=token)
 
-    token = util.prompt_for_user_token(username, scope)
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
-
-        tracks = sp.album_tracks('44aVVevMGKS4q6nWXbELpP')
-        for track in tracks['items']:
-            print track['name']
-            features = sp.audio_features(str(track['id']))
-            print features[0]['key']
-            #recommendations = sp.recommendations(str(track['id']))
-            #print recommendations
-        if len(sys.argv) < 2:
-            print 'Usage: {0} artist name'.format(sys.argv[0])
+    tracks = sp.album_tracks(album_id)
+    ordered = []
+    for track in tracks['items']:
+        features = sp.audio_features(str(track['id']))
+        energy = features[0]['energy']
+        if len(ordered) == 0:
+            ordered.append((str(track['id']), energy))
         else:
-            name = ' '.join(sys.argv[1:])
-            artist = get_artist(name)
-            if artist:
-                show_artist_albums(artist)
-            else:
-                print("Can't find that artist")
+            for i in range(0, len(ordered)):
+                if ordered[i][1] < energy:
+                    ordered.insert(i, (str(track['id']), energy))
+                    break
+    track_ids = [track[0] for track in ordered]
+    sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+
+        #recommendations = sp.recommendations(str(track['id']))
+        #print recommendations
